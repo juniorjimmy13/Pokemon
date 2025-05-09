2deb62c665099183ef54aacead10b321062eb25a
jimmyjr <juniorjimmy13@gmail.com>
2025-05-09 17:35:47 +0300

    Feature 1- Oak and Pokémon selection

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -4,8 +4,36 @@ using namespace std;
 int main() {
 
     string playerName;
+    int choice = 0;
     cout << "What is your name : ";
     cin >> playerName;
-    cout << "Great Start " << playerName << " Welcome to the game";
+    cout << playerName << ", Welcome to the Pokemon Centre I'm professor oak\n";
+
+
+    cout <<" There are so many Pokemon to choose from Which would you like\n";
+    cout << "For now I have three starter types for a young trainer such as yourself\n";
+    cout << "1. Bulbasaur\n2. Squirtle\n3. Charmander\n";
+    cout << "Which one would you like ? \n Choose a number : ";
+
+    cin >> choice;
+
+    if (choice == 1)
+    {
+        cout << "Bulbasaur! Great choice a strong earth type be careful not to cause an earthquake";
+    }
+    else if (choice == 2)
+    {
+        cout << "Squirlte! What a perfect specimen , always handy when there's a fire around";
+    }
+    else if (choice == 3)
+    {
+        cout << "Charmander! The great fire breather, lets not burn down the centre just yet";
+    }
+    else 
+    { 
+        cout << "Invalid choice, Restart game"; 
+    }
+
+
     return 0;
 }
