5b371269b53ca525b502663ddd924afa3070e1bc
jimmyjr <juniorjimmy13@gmail.com>
2025-05-09 17:18:34 +0300

    Feature_1_Intro

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -6,6 +6,6 @@ int main() {
     string playerName;
     cout << "What is your name : ";
     cin >> playerName;
-    cout << "Great Start " << playerName << " looks like you have understood the main() function properly now!";
+    cout << "Great Start " << playerName << " Welcome to the game";
     return 0;
 }
