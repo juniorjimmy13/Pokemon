1f342d6f75a7556f7850b97d7c4be6e501d70ea0
jimmyjr <juniorjimmy13@gmail.com>
2025-05-09 19:06:10 +0300

    Added Enums

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
+enum pokemonChoice
+{
+    Squirtle,
+    Bulbasaur,
+    Charmander,
+    invalidChoice
+};
 int main() {
 
     string playerName;
-    string chosen_pokemon;
+    pokemonChoice chosen_pokemon = invalidChoice;
     int choice = 0;
     cout << "What is your name : ";
     cin >> playerName;
@@ -21,27 +28,43 @@ int main() {
     switch (choice)
     {
     case 1:
-        cout << "Professor Oak: Bulbasaur! Great choice a strong earth type be careful not to cause an earthquake";
-        chosen_pokemon = "Bulbasaur";
+        chosen_pokemon = Bulbasaur;
         break;
     case 2:
-        cout << "Professor Oak: Squirlte! What a perfect specimen , always handy when there's a fire around";
-        chosen_pokemon = "Squirtle";
+        chosen_pokemon = Squirtle;
         break;
 
     case 3:
-        cout << "Professor Oak: Charmander! The great fire breather, lets not burn down the centre just yet";
-        chosen_pokemon = "Charmander";
+        
+        chosen_pokemon = Charmander;
         break;
 
     default:
         cout << "Professor Oak: Hmm, that doesn't seem right. Let me choose for you...\n";
         cout << "Professor Oak: Just kidding! Let's go with Pikachu Suprise!!!";
-        chosen_pokemon = "Pikachu";
+        chosen_pokemon = invalidChoice;
         break;
     }
-    cout << " an excellent choice!\nBut beware, Trainer,\nthis is only the beginning.\nYour journey is about to unfold.\nNow let’s see if you’ve got what it takes to keep going!\nGood luck, and remember… Choose wisely!";
 
+    switch (chosen_pokemon)
+    {
+    case Squirtle:
+        cout << "Professor Oak: Squirlte! What a perfect specimen , always handy when there's a fire around";
+        break;
+    case Bulbasaur:
+        cout << "Professor Oak: Bulbasaur! Great choice a strong earth type be careful not to cause an earthquake";
+        break;
+    case Charmander:
+        cout << "Professor Oak: Charmander! The great fire breather, lets not burn down the centre just yet";
+        break;
+    case invalidChoice:
+        cout << "Professor Oak: Hmm, that doesn't seem right. Let me choose for you...\n";
+        cout << "Professor Oak: Just kidding! Let's go with Charmander";
+        chosen_pokemon = Charmander;
+        break;
+    }
+    cout << " an excellent choice!\nBut beware, Trainer,\nthis is only the beginning.\nYour journey is about to unfold.";
 
+    cout << "You and " << (chosen_pokemon == Charmander ? "Charmander" : chosen_pokemon == Bulbasaur ? "Bulbasaur" : "Squirtle") << " Will make a great Team";
     return 0;
 }
