31711e2c6438a14364aa9126ea5f08631841bb61
jimmyjr <juniorjimmy13@gmail.com>
2025-05-09 19:11:06 +0300

    Addition of Enum Classes

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 using namespace std;
 
-enum pokemonChoice
+enum class pokemonChoice
 {
     Squirtle,
     Bulbasaur,
     Charmander,
     invalidChoice
 };
+enum class pokemonType
+{
+    Fire,
+    Water,
+    Normal,
+    Earth,
+    Electric
+};
 int main() {
 
     string playerName;
-    pokemonChoice chosen_pokemon = invalidChoice;
+    pokemonChoice chosen_pokemon = pokemonChoice::invalidChoice;
     int choice = 0;
     cout << "What is your name : ";
     cin >> playerName;
@@ -28,43 +36,43 @@ int main() {
     switch (choice)
     {
     case 1:
-        chosen_pokemon = Bulbasaur;
+        chosen_pokemon = pokemonChoice::Bulbasaur;
         break;
     case 2:
-        chosen_pokemon = Squirtle;
+        chosen_pokemon = pokemonChoice::Squirtle;
         break;
 
     case 3:
         
-        chosen_pokemon = Charmander;
+        chosen_pokemon = pokemonChoice::Charmander;
         break;
 
     default:
         cout << "Professor Oak: Hmm, that doesn't seem right. Let me choose for you...\n";
         cout << "Professor Oak: Just kidding! Let's go with Pikachu Suprise!!!";
-        chosen_pokemon = invalidChoice;
+        chosen_pokemon = pokemonChoice::invalidChoice;
         break;
     }
 
     switch (chosen_pokemon)
     {
-    case Squirtle:
+    case pokemonChoice::Squirtle:
         cout << "Professor Oak: Squirlte! What a perfect specimen , always handy when there's a fire around";
         break;
-    case Bulbasaur:
+    case pokemonChoice::Bulbasaur:
         cout << "Professor Oak: Bulbasaur! Great choice a strong earth type be careful not to cause an earthquake";
         break;
-    case Charmander:
+    case pokemonChoice::Charmander:
         cout << "Professor Oak: Charmander! The great fire breather, lets not burn down the centre just yet";
         break;
-    case invalidChoice:
+    case pokemonChoice::invalidChoice:
         cout << "Professor Oak: Hmm, that doesn't seem right. Let me choose for you...\n";
         cout << "Professor Oak: Just kidding! Let's go with Charmander";
-        chosen_pokemon = Charmander;
+        chosen_pokemon = pokemonChoice::Charmander;
         break;
     }
     cout << " an excellent choice!\nBut beware, Trainer,\nthis is only the beginning.\nYour journey is about to unfold.";
 
-    cout << "You and " << (chosen_pokemon == Charmander ? "Charmander" : chosen_pokemon == Bulbasaur ? "Bulbasaur" : "Squirtle") << " Will make a great Team";
+    cout << "You and " << (chosen_pokemon == pokemonChoice::Charmander ? "Charmander" : chosen_pokemon == pokemonChoice::Bulbasaur ? "Bulbasaur" : "Squirtle") << " Will make a great Team";
     return 0;
 }
