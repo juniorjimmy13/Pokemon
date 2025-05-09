4116446633f95d4d5f7786ba8da59eb2014f61b2
jimmyjr <juniorjimmy13@gmail.com>
2025-05-09 17:15:31 +0300

    added request input for player name and output message

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main() {
 
+    string playerName;
+    cout << "What is your name : ";
+    cin >> playerName;
+    cout << "Great Start " << playerName << " looks like you have understood the main() function properly now!";
     return 0;
 }
